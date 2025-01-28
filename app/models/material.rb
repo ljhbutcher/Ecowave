026f57992111441f7b2617f021b1943397c40bb6
Sapana Fisher <187521469+sapana556@users.noreply.github.com>
2025-01-28 17:21:54 +0000

    updated homepage

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,17 @@
 class Material < ApplicationRecord
   has_many :projects, through: :project_material
   has_one_attached :photo
+
+  def self.avg_electricity
+    average(:electricity_used)
+  end
+
+  def self.avg_water
+    average(:water_usage)
+  end
+
+  def self.avg_co2
+    average(:CO2)
+  end
+
 end
