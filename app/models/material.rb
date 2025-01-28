69984e9857e3efd73261bbc4ed38e493a1c7c390
Melany Sierra <melanysabrina@icloud.com>
2025-01-28 18:49:14 +0000

    included more associations

--- app/models/material.rb
+++ app/models/material.rb
@@ -1,4 +1,5 @@
 class Material < ApplicationRecord
   has_many :projects, through: :project_material
+  has_many :project_materials, dependent: :destroy
   has_one_attached :photo
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
-  has_many :materials, through: :project
+  has_many :materials, through: :project_materials
+  has_many :project_materials, dependent: :destroy
 end
