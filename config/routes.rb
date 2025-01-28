1d435e90a1b37e99fb2a328d6003990b425a7ae5
Liam Butcher <liam_butcher@hotmail.co.uk>
2025-01-28 15:04:26 +0000

    log in page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#landing_page"
   get "/sign_in", to: "sessions#new"
   get "/sign_up", to: "users#new"
+  delete "/users/sign_out", to: "sessions#destroy"
 
   resources :projects do
     resources :project_materials, only: [:new, :create]
