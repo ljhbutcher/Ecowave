8d6ec6e3386238d6248bff0e3abed46a3f9e789a
Sapana Fisher <187521469+sapana556@users.noreply.github.com>
2025-02-03 10:51:29 +0000

    AI chat integrated (but need to refine prompt)

---
+++ app/jobs/chatbot_job.rb
@@ -0,0 +1,42 @@
+class ChatbotJob < ApplicationJob
+  queue_as :default
+
+  def perform(question)
+    @question = question
+    chatgpt_response = client.chat(
+      parameters: {
+        model: "gpt-4o-mini",
+        messages: questions_formatted_for_openai # to code as private method
+      }
+    )
+    new_content = chatgpt_response["choices"][0]["message"]["content"]
+
+    question.update(ai_answer: new_content)
+    Turbo::StreamsChannel.broadcast_update_to(
+      "question_#{@question.id}",
+      target: "question_#{@question.id}",
+      partial: "questions/question", locals: { question: question })
+  end
+
+  private
+
+  def client
+    @client ||= OpenAI::Client.new
+  end
+
+  # def perform(*args)
+  #   # Do something later
+  # end
+
+  def questions_formatted_for_openai
+    questions = @question.user.questions
+    results = []
+    results << { role: "system", content: "You are an assistant for a website that aims to inform fashion industry experts on the environmental impact of the materials they use." }
+    questions.each do |question|
+      results << { role: "user", content: question.user_question }
+      results << { role: "assistant", content: question.ai_answer || "" }
+    end
+    return results
+  end
+
+end
