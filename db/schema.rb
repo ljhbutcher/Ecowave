02aac2505523a3ddc6cfd5a45326bf42d626f320
edwardmwinasalexander <edward.m.alexander@hotmail.com>
2025-02-06 12:46:59 +0000

    new deadline

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_04_130748) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_06_115655) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -96,7 +96,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_04_130748) do
     t.datetime "updated_at", null: false
     t.text "summary"
     t.text "notes"
-    t.datetime "deadline"
+    t.date "deadline"
   end
 
   create_table "questions", force: :cascade do |t|
