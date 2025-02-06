c0d795aecd8b0d7bed6f89d583828a35f9b0c555
Marcel Kedziora <nuage.clan@gmail.com>
2025-02-06 17:20:56 +0000

    tweaks here and there

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,47 +1,46 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
     if params[:query].present?
       @projects = Project.where("name ILIKE ? OR description ILIKE ?", "%#{params[:query]}%", "%#{params[:query]}%")
-    else
-      @projects = Project.all
     end
     params[:query] = ""
   end
 
   def show
-    @project = Project.find(params[:id])
     @materials = @project.materials
     @material = @materials.first
     @project_materials = @project.project_materials.includes(:material)
     @project_material = ProjectMaterial.new
   end
 
-
   def new
     @project = Project.new
-
   end
 
   def create
     @project = Project.new(project_params)
+
+    # Ensure summary gets updated if not manually provided
+    @project.summary = @project.description if @project.summary.blank?
+
     if @project.save
-      redirect_to project_path(@project)
+      redirect_to project_path(@project), notice: "Project successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       respond_to do |format|
-        format.html { redirect_to @project, notice: "Summary updated successfully." }
-        format.json { render json: { summary: @project.summary, notes: @project.notes} }
+        format.html { redirect_to @project, notice: "Project updated successfully." }
+        format.json { render json: { summary: @project.summary, notes: @project.notes } }
       end
     else
       render :show, status: :unprocessable_entity
@@ -49,15 +48,17 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
-    redirect_to projects_path, notice: 'Project was successfully destroyed.'
+    redirect_to projects_path, notice: 'Project was successfully deleted.'
   end
 
   private
 
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
   def project_params
     params.require(:project).permit(:name, :description, :summary, :notes, :deadline, :photo)
   end
-
 end
